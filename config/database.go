9f4e6ef693e9c58507eb4567a5e1515f5bbdea06
hemavathi15sg <hemavathi.n@ecanarys.com>
2026-01-21 15:37:42 +0530

    feat: add database configuration with connection details

---
+++ config/database.go
@@ -0,0 +1,9 @@
+package config
+const (
+	dbhost	 = "localhost"
+	dbport	 = "5432"
+	dbuser	 = "admin"
+	dbpassword = "glc_eyJvIjoiMTYzMTM5MSIsIm4iOiJzdGFjay0xNDg0MzE0LWFsbG95LWluc3RhZ3JhbSIsImsiOiI1NVMyMEdFRzZOM3JKRzBxWTY0TUM0bGUiLCJtIjp7InIiOiJwcm9kLWFwLXNvdXRoLTEifX0= 
+"
+	dbname	 = "grademanagement"
+)
